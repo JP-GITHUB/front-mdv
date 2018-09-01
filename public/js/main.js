6746b567ddba40902d5eb67b1391679e0ad55bcd
jpulgarp <joseluis.pulgarperez@cencosud.cl>
2018-08-31 22:04:27 -0300

    Add request api

--- public/js/main.js
+++ public/js/main.js
@@ -12,4 +12,45 @@ $(document).ready(() => {
 
         $("#wrapper ").toggleClass("toggled ");
     });
+
+    var pageObject = {
+        apiUrl: 'http://localhost:3001',
+
+        myMethod: function () {
+            console.log(myFeature.myProperty);
+        },
+
+        getSchools: function () {
+            $.ajax({
+                url: this.apiUrl + '/schools',
+                method: 'GET',
+                dataType: 'JSON',
+                success: function (obj) {
+                    let tmp_menu_school = '';
+                    
+                    $.each(obj['schools'], function (index, data) {
+                        
+                        tmp_menu_school += '<a class="dropdown-item" href="/catalogo">' + data.name + '</a><div class="dropdown-divider"></div>';
+                    });
+
+                    $("#nav-app .dropdown-menu").html(tmp_menu_school);
+                },
+                error: function (err) {
+
+                }
+            });
+        },
+
+        init: function (settings) {
+            //myFeature.settings = settings;
+            this.getSchools();
+        },
+
+        readSettings: function () {
+            console.log(myFeature.settings);
+        }
+    };
+
+    pageObject.init();
+
 });
