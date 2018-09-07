784d4a8a0b40f0c9da41965e6199931ae5bfe289
jpulgarp <joseluis.pulgarperez@cencosud.cl>
2018-09-07 02:24:31 -0300

    eventos boton datatables, modal

--- public/js/perfiles.js
+++ public/js/perfiles.js
@@ -1,6 +1,11 @@
 $(document).ready(() => {
     var objPerfiles = {
         apiUrl: 'http://localhost:3001',
+
+        editPerfil: function () {
+
+        },
+
         init_datatables: function () {
             let table = $('#table_perfiles').DataTable({
                 "ajax": 'http://localhost:3001/perfiles',
@@ -12,19 +17,25 @@ $(document).ready(() => {
                     { "data": "updated_at" },
                     {
                         mRender: function (data, type, row) {
-                            var linkEdit = '<a class="table-edit" data-id="' + row[0] + '">Editar</a>';
+                            var linkEdit = '<a class="table-edit btn-edit" data-id="' + row.id + '" data-toggle="modal" data-target="#edit_modal">Editar</a>';
                             linkEdit = linkEdit.replace("-1", row.ID);
 
-                            var linkDetails = '<a class="table-detail" data-id="' + row[0] + '">Detalle</a>';
+                            var linkDetails = '<a class="btn-details" data-id="' + row.id + '">Detalle</a>';
                             linkDetails = linkDetails.replace("-1", row.ID);
 
-                            var linkDelete = '<a class="table-delete" data-id="' + row[0] + '">Eliminar</a>';
+                            var linkDelete = '<a class="table-delete btn-delete" data-id="' + row.id + '">Eliminar</a>';
                             linkDelete = linkDelete.replace("-1", row.ID);
 
                             return linkDetails + " | " + linkEdit + " | " + linkDelete;
                         }
                     }
                 ],
+            });
+
+            $('#table_perfiles tbody').on('click', 'tr', function () {
+                let data = table.row(this).data();
+                $("#txt_nombre").val(data.nombre);
+                $("#cbx_estado").val((data.estado) ? 1 : 2);
             });
         },
 
