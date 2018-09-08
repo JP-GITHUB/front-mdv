e1fcef2d0d15a712f291a973a16c664fee13d131
jpulgarp <joseluis.pulgarperez@cencosud.cl>
2018-09-08 01:15:29 -0300

    Refresh table delete

--- public/js/usuarios.js
+++ public/js/usuarios.js
@@ -11,26 +11,26 @@ $(document).ready(() => {
                     { "data": "rut" },
                     { "data": "mail" },
                     { "data": "telefono" },
-                    { "data": "password"},
+                    { "data": "password" },
                     { "data": "estado" },
-                    { 
+                    {
                         render: function (data, type, row) { return FormatoFecha(row.updated_at); }
                     },
                     {
                         mRender: function (data, type, row) {
-                            var linkEdit = '<button type="button" class="btn btn-success"'+
-                            'onclick="LoadModal('+row.id+',\''+ row.nombre+'\',\''+row.apellido+'\',\''+row.rut+'\',\''+row.mail+'\'' +
-                            ',\''+row.telefono+'\',\''+row.password+'\',\''+row.estado+'\')"'+
-                            'data-toggle="modal" data-target="#edit_modal">' +
-                            '<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>Editar</button>';
+                            var linkEdit = '<button type="button" class="btn btn-success"' +
+                                'onclick="LoadModal(' + row.id + ',\'' + row.nombre + '\',\'' + row.apellido + '\',\'' + row.rut + '\',\'' + row.mail + '\'' +
+                                ',\'' + row.telefono + '\',\'' + row.password + '\',\'' + row.estado + '\')"' +
+                                'data-toggle="modal" data-target="#edit_modal">' +
+                                '<span class="glyphicon glyphicon-ok" aria-hidden="true"></span>Editar</button>';
                             linkEdit = linkEdit.replace("-1", row.ID);
 
                             var linkDetails = '<a class="table-detail" data-id="' + row.id + '">Detalle</a>';
                             linkDetails = linkDetails.replace("-1", row.ID);
 
-                            var linkDelete = '<button type="button" id="dtBtonoRechazar" class="btn btn-danger" onclick="LoadDelete('+row.id+')"'+
-                            'data-toggle="modal" data-target="#delete_modal">' +
-                            '<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>Eliminar</button>';
+                            var linkDelete = '<button type="button" id="dtBtonoRechazar" class="btn btn-danger" onclick="LoadDelete(' + row.id + ')"' +
+                                'data-toggle="modal" data-target="#delete_modal">' +
+                                '<span class="glyphicon glyphicon-remove" aria-hidden="true"></span>Eliminar</button>';
                             linkDelete = linkDelete.replace("-1", row.ID);
 
                             return /*linkDetails + " | " + */linkEdit + " | " + linkDelete;
@@ -40,15 +40,15 @@ $(document).ready(() => {
                 "responsive": true
 
             });
-            $('a.toggle-vis').on( 'click', function (e) {
+            $('a.toggle-vis').on('click', function (e) {
                 e.preventDefault();
-         
+
                 // Get the column API object
-                var column = table.column( $(this).attr('data-column') );
-         
+                var column = table.column($(this).attr('data-column'));
+
                 // Toggle the visibility
-                column.visible( ! column.visible() );
-            } );
+                column.visible(!column.visible());
+            });
         },
 
         init: function (settings) {
@@ -64,59 +64,59 @@ $(document).ready(() => {
 
 });
 
-function LoadModal(id, nombre, apellido, rut, mail, telefono, password, estado){
+function LoadModal(id, nombre, apellido, rut, mail, telefono, password, estado) {
     var modal_ini = '<div id="edit_modal" class="modal" tabindex="-1" role="dialog">';
     $("#edit_modal").html(modal_ini);
-    var content = ''+
-    '<div class="modal-dialog" role="document">'+
-        '<div class="modal-content">'+
-            '<div class="modal-header">'+
-                '<h5 class="modal-title">Panel de edición</h5>'+
-                '<button type="button" class="close" data-dismiss="modal" aria-label="Close">'+
-                    '<span aria-hidden="true">&times;</span>'+
-                '</button>'+
-            '</div>'+
-            '<div class="modal-body">'+
-                '<form>'+
-                    '<div class="form-group">'+
-                        '<label for="Nombre">Nombre</label>'+
-                        '<input type="text" class="form-control" id="txt_nombre" value="'+nombre+'">'+
-                    '</div>'+
-                    '<div class="form-group">'+
-                        '<label for="Apellido">Apellido</label>'+
-                        '<input type="text" class="form-control" id="txt_apellido" value="'+apellido+'">'+
-                    '</div>'+
-                    '<div class="form-group">'+
-                        '<label for="Rut">Rut</label>'+
-                        '<input type="text" class="form-control" id="txt_rut" value="'+rut+'">'+
-                    '</div>'+
-                    '<div class="form-group">'+
-                        '<label for="Mail">Mail</label>'+
-                        '<input type="text" class="form-control" id="txt_mail" value="'+mail+'">'+
-                    '</div>'+
-                    '<div class="form-group">'+
-                        '<label for="Telefono">Telefono</label>'+
-                        '<input type="text" class="form-control" id="txt_telefono" value="'+telefono+'">'+
-                    '</div>'+
-                    '<div class="form-group">'+
-                        '<label for="Contraseña">Contraseña</label>'+
-                        '<input type="text" class="form-control" id="txt_contraseña" value="'+password+'">'+
-                    '</div>'+
-                '</form>'+
-            '</div>'+
-            '<div class="modal-footer">'+
-                '<button type="button" class="btn btn-primary" onclick=UpdateUser('+id+') data-toggle="modal" data-target="#edit_modal">Save changes</button>'+
-                '<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>'+
-            '</div>'+
-        '</div>'+
-    '</div>'
-    
+    var content = '' +
+        '<div class="modal-dialog" role="document">' +
+        '<div class="modal-content">' +
+        '<div class="modal-header">' +
+        '<h5 class="modal-title">Panel de edición</h5>' +
+        '<button type="button" class="close" data-dismiss="modal" aria-label="Close">' +
+        '<span aria-hidden="true">&times;</span>' +
+        '</button>' +
+        '</div>' +
+        '<div class="modal-body">' +
+        '<form>' +
+        '<div class="form-group">' +
+        '<label for="Nombre">Nombre</label>' +
+        '<input type="text" class="form-control" id="txt_nombre" value="' + nombre + '">' +
+        '</div>' +
+        '<div class="form-group">' +
+        '<label for="Apellido">Apellido</label>' +
+        '<input type="text" class="form-control" id="txt_apellido" value="' + apellido + '">' +
+        '</div>' +
+        '<div class="form-group">' +
+        '<label for="Rut">Rut</label>' +
+        '<input type="text" class="form-control" id="txt_rut" value="' + rut + '">' +
+        '</div>' +
+        '<div class="form-group">' +
+        '<label for="Mail">Mail</label>' +
+        '<input type="text" class="form-control" id="txt_mail" value="' + mail + '">' +
+        '</div>' +
+        '<div class="form-group">' +
+        '<label for="Telefono">Telefono</label>' +
+        '<input type="text" class="form-control" id="txt_telefono" value="' + telefono + '">' +
+        '</div>' +
+        '<div class="form-group">' +
+        '<label for="Contraseña">Contraseña</label>' +
+        '<input type="text" class="form-control" id="txt_contraseña" value="' + password + '">' +
+        '</div>' +
+        '</form>' +
+        '</div>' +
+        '<div class="modal-footer">' +
+        '<button type="button" class="btn btn-primary" onclick=UpdateUser(' + id + ') data-toggle="modal" data-target="#edit_modal">Save changes</button>' +
+        '<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>' +
+        '</div>' +
+        '</div>' +
+        '</div>'
+
     $("#edit_modal").append(content);
 };
 
-function UpdateUser(id){
+function UpdateUser(id) {
     let table_instance = $('#table_perfiles').DataTable();
-    
+
     let nombre = $("#txt_nombre").val();
     let apellido = $("#txt_apellido").val();
     let rut = $("#txt_rut").val();
@@ -125,8 +125,8 @@ function UpdateUser(id){
     let password = $("#txt_contraseña").val();
 
     $.ajax({
-        method:"PUT",
-        data:{
+        method: "PUT",
+        data: {
             id: id,
             nombre: nombre,
             apellido: apellido,
@@ -136,17 +136,17 @@ function UpdateUser(id){
             password: password
         },
         dataType: "json",
-        url:"http://localhost:3001/usuarios",
-        success: function(){
+        url: "http://localhost:3001/usuarios",
+        success: function () {
             table_instance.ajax.reload();
         },
         error: function (err) {
-            
+
         }
     });
 }
 
-function FormatoFecha(fecha) {   
+function FormatoFecha(fecha) {
     //DECLARACION
     var date = "";
     var año = "";
@@ -172,44 +172,46 @@ function FormatoFecha(fecha) {
     return formatoFecha;
 };
 
-function LoadDelete(id){
+function LoadDelete(id) {
     var modal_ini = '<div id="delete_modal" class="modal" tabindex="-1" role="dialog">';
     $("#delete_modal").html(modal_ini);
-    var content = ''+
-    '<div class="modal-dialog" role="document">'+
-        '<div class="modal-content">'+
-            '<div class="modal-header">'+
-                '<h5 class="modal-title">Confirmación</h5>'+
-                '<button type="button" class="close" data-dismiss="modal" aria-label="Close">'+
-                    '<span aria-hidden="true">&times;</span>'+
-                '</button>'+
-            '</div>'+
-            '<div class="modal-body">'+
-                '<h4>¿Esta seguro que desea eliminar el registro?</h4>'+
-            '</div>'+
-            '<div class="modal-footer">'+
-                '<button type="button" class="btn btn-danger" onclick=Delete('+id+') data-toggle="modal" data-target="#delete_modal">Eliminar</button>'+
-                '<button type="button" class="btn btn-success" data-dismiss="modal">Cerrar</button>'+
-            '</div>'+
-        '</div>'+
-    '</div>'
-    
+    var content = '' +
+        '<div class="modal-dialog" role="document">' +
+        '<div class="modal-content">' +
+        '<div class="modal-header">' +
+        '<h5 class="modal-title">Confirmación</h5>' +
+        '<button type="button" class="close" data-dismiss="modal" aria-label="Close">' +
+        '<span aria-hidden="true">&times;</span>' +
+        '</button>' +
+        '</div>' +
+        '<div class="modal-body">' +
+        '<h4>¿Esta seguro que desea eliminar el registro?</h4>' +
+        '</div>' +
+        '<div class="modal-footer">' +
+        '<button type="button" class="btn btn-danger" onclick=Delete(' + id + ') data-toggle="modal" data-target="#delete_modal">Eliminar</button>' +
+        '<button type="button" class="btn btn-success" data-dismiss="modal">Cerrar</button>' +
+        '</div>' +
+        '</div>' +
+        '</div>'
+
     $("#delete_modal").append(content);
 };
 
-function Delete(id){
+function Delete(id) {
+    let table_instance = $('#table_perfiles').DataTable();
+
     $.ajax({
-        method:"DELETE",
-        data:{
+        method: "DELETE",
+        data: {
             id: id
         },
         dataType: "json",
-        url:"http://localhost:3001/usuarios",
-        success: function(){
-            
+        url: "http://localhost:3001/usuarios",
+        success: function () {
+            table_instance.ajax.reload();
         },
         error: function (err) {
-            
+
         }
     });
 };
